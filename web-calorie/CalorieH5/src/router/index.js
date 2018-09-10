d16aa6cffb28e0964d3b147d13dcf27b40f03222
Liuxy <liuxy0551@qq.com>
2018-09-10 16:20:00 +0800

    去除不需要的部分

--- web-calorie/CalorieH5/src/router/index.js
+++ web-calorie/CalorieH5/src/router/index.js
@@ -7,28 +7,12 @@ Vue.use(Router)
 
 export const constantRouterMap = [
   { path: '/login', component: () => import('../pages/login/login'), hidden: true },
-  // { path: '/register', component: () => import('../pages/login/register'),hidden: true },
-  // { path: '/search', component: () => import('../pages/index/search'), hidden: true },
-  // { path: '/product', component: () => import('../pages/index/product'),hidden: true },
   {
     path: '/',
     component: Layout,
     redirect: 'login',
-    children: [{ path: '/login', component: () => import('../pages/login/login'), hidden: true }
-    ],
+    children: [{ path: '/login', component: () => import('../pages/login/login'), hidden: true }],
   },
-  /*{
-    path: '/index',
-    component: Layout,
-    redirect: 'index/index',
-    children: [{
-      path: 'index',
-      component: () => import('../pages/index/index'),
-      name: 'index',
-      meta: { title: 'index', icon: 'index', noCache: true }
-    }
-    ],
-  },*/
   {
     path: '/diary',
     component: Layout,
@@ -38,9 +22,7 @@ export const constantRouterMap = [
       component: () => import('../pages/diary/index'),
       name: 'diary',
       meta: { title: 'diary', icon: 'diary', noCache: true }
-    }
-    ],
-
+    }],
   },
   {
     path: '/meal',
@@ -51,9 +33,7 @@ export const constantRouterMap = [
       component: () => import('../pages/meal/index'),
       name: 'meal',
       meta: { title: 'meal', icon: 'meal', noCache: true }
-    }
-    ],
-
+    }],
   },
   {
     path: '/interaction',
@@ -64,9 +44,7 @@ export const constantRouterMap = [
       component: () => import('../pages/interaction/index'),
       name: 'interaction',
       meta: { title: 'interaction', icon: 'interaction', noCache: true }
-    }
-    ],
-
+    }],
   },
   {
     path: '/personal',
@@ -77,11 +55,9 @@ export const constantRouterMap = [
       component: () => import('../pages/personal/index'),
       name: 'personal',
       meta: { title: 'personal', icon: 'personal', noCache: true }
-    }
-    ],
+    }],
   },
 
-//  我的
   /*账号设置*/
   // { path: '/setUp', component: () => import('../pages/personal/setUp/index'), hidden: true },
 
