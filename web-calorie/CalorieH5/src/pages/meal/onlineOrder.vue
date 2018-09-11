87e20ee7e006abbcad088a35bf3f305a9ea06d88
Liuxy <liuxy0551@qq.com>
2018-09-11 16:29:00 +0800

    铺页面

--- web-calorie/CalorieH5/src/pages/meal/onlineOrder.vue
+++ web-calorie/CalorieH5/src/pages/meal/onlineOrder.vue
@@ -1,11 +1,44 @@
 <template>
-  <div>
+  <div style="margin-bottom: 50px">
     <div class="mt-swipe-box">
       <mt-swipe :auto="15000">
         <mt-swipe-item v-for="item in bannerList" :key="item.id">
           <img :src="item.url" class="img">
         </mt-swipe-item>
       </mt-swipe>
+    </div>
+
+    <div class="cook-style">
+      <ul class="cook-style-list">
+        <li class="cook-style-box" v-for="(item, index) in cookStyleList" @click="styleChoose(item, index)">
+          <img class="cook-style-img" :class="index == style_select?'active':''" :src="item.src" alt="">
+          <div class="cook-style-name">{{item.name}}</div>
+        </li>
+      </ul>
+    </div>
+
+    <div class="food-box" v-for="item in foodList">
+      <img class="food-img" :src="item.src" alt="">
+      <div class="food-box-right">
+        <div class="right-row">
+          <div class="food-name">{{item.name}}</div>
+          <div class="food-price">￥{{item.price}}</div>
+        </div>
+        <div class="right-row">
+          <div class="food-rate">好评率：{{item.rate}}%</div>
+          <div class="food-old-price">￥{{item.oldPrice}}</div>
+        </div>
+      </div>
+    </div>
+
+    <div class="to-order">
+      <div class="cart-product-box">
+        <img class="cart-img" src="/static/images/purple/meal_shop_cart.png" alt="">
+        <div class="cart-product-num">3</div>
+      </div>
+      <div class="cart-text">合计</div>
+      <div class="cart-price">￥18.0</div>
+      <div class="to-order-btn">去 下 单</div>
     </div>
   </div>
 </template>
@@ -20,11 +53,35 @@
           { url: 'http://img4.imgtn.bdimg.com/it/u=632938294,2120575488&fm=11&gp=0.jpg' },
           { url: 'http://img.zcool.cn/community/01a16c57fb3e1aa84a0d304fc108d5.jpg@2o.jpg' }
         ],
+        style_select: 0,
+        cookStyleList: [
+          { src: "http://himg.china.cn/0/4_203_120316_750_750.jpg", name: "全部" },
+          { src: "http://himg.china.cn/0/4_203_120316_750_750.jpg", name: "包子粥类" },
+          { src: "http://himg.china.cn/0/4_203_120316_750_750.jpg", name: "面条米线" },
+          { src: "http://himg.china.cn/0/4_203_120316_750_750.jpg", name: "川菜" },
+          { src: "http://himg.china.cn/0/4_203_120316_750_750.jpg", name: "粤菜" },
+          { src: "http://himg.china.cn/0/4_203_120316_750_750.jpg", name: "肉类" },
+          { src: "http://himg.china.cn/0/4_203_120316_750_750.jpg", name: "蔬菜" },
+          { src: "http://himg.china.cn/0/4_203_120316_750_750.jpg", name: "水果" }
+        ],
+        foodList: [
+          { src: "http://himg.china.cn/0/4_203_120316_750_750.jpg", name: "草莓甜点", price: "10.00", oldPrice: "12.00", rate: "95.8", inventory: "0" },
+          { src: "http://himg.china.cn/0/4_203_120316_750_750.jpg", name: "草莓甜点", price: "10.00", oldPrice: "12.00", rate: "95.8", inventory: "10" },
+          { src: "http://himg.china.cn/0/4_203_120316_750_750.jpg", name: "草莓甜点", price: "10.00", oldPrice: "12.00", rate: "95.8", inventory: "10" },
+          { src: "http://himg.china.cn/0/4_203_120316_750_750.jpg", name: "草莓甜点", price: "10.00", oldPrice: "12.00", rate: "95.8", inventory: "10" },
+          { src: "http://himg.china.cn/0/4_203_120316_750_750.jpg", name: "草莓甜点", price: "10.00", oldPrice: "12.00", rate: "95.8", inventory: "10" },
+          { src: "http://himg.china.cn/0/4_203_120316_750_750.jpg", name: "草莓甜点", price: "10.00", oldPrice: "12.00", rate: "95.8", inventory: "10" },
+          { src: "http://himg.china.cn/0/4_203_120316_750_750.jpg", name: "草莓甜点", price: "10.00", oldPrice: "12.00", rate: "95.8", inventory: "10" }
+        ]
       }
     },
     components: {  },
     methods: {
-
+      // 菜品种类选择
+      styleChoose(item, index) {
+        this.style_select = index;
+        // console.log(item, index);
+      }
     },
     mounted() {
 
@@ -32,18 +89,7 @@
   }
 </script>
 
-<style lang="less" rel="stylesheet/less">
-  @import "../../common/css/index";
+<style lang="less" rel="stylesheet/less" scoped>
+  @import "../../common/css/_order";
 
-  .mt-swipe-box {
-    /*margin-bottom: 130px;*/
-    height: 300px;
-    box-shadow: 0 5px 6px 0 rgba(0, 0, 0, 0.16);
-    .img {
-      height: 300px;
-    }
-    .mint-swipe-indicators {
-      top: 250px;
-    }
-  }
 </style>
