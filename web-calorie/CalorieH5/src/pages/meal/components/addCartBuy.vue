c1777edd0ac2766f9e2b4681c5c4e02beca20818
liuxy0551 <liuxy0551@qq.com>
2018-09-13 20:36:45 +0800

    可选择的购物车组件

---
+++ web-calorie/CalorieH5/src/pages/meal/components/addCartBuy.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div class="add-cart-buy">
+      <img class="cart-img" src="/static/images/purple/meal_shop_cart.png" alt="">
+      <div class="add-cart m-text">加入购物车</div>
+      <div class="buy-now m-text">立即购买</div>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+
+  export default {
+    name: "addCartBuy",
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    mounted() {
+
+    }
+  }
+</script>
+
+<style lang="less" rel="stylesheet/less" scoped>
+  @import "../../../common/css/index";
+
+  .add-cart-buy {
+    .cart-img {
+      width: 88px;
+      height: 88px;
+    }
+    .add-cart {
+
+    }
+    .buy-now {
+
+    }
+    m-text {
+      width: 150px;
+      height: 40px;
+      font-size: 30px;
+      color: @white;
+    }
+  }
+</style>
--- web-calorie/CalorieH5/src/pages/meal/components/cartChoose.vue
+++ web-calorie/CalorieH5/src/pages/meal/components/cartChoose.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    123456
+    <div class="add-cart-buy">
+      <img class="choose-all-img" src="" alt="">
+      <div class="m-ft-24 m-hex">全选</div>
+      <div class="m-ft-24 m-hex">合计
+        <span class="m-ft-30 m-price m-ft-b">￥193.02</span>
+      </div>
+      <div class="buy-btn">立即购买</div>
+    </div>
   </div>
 </template>
 
@@ -23,6 +30,19 @@
 </script>
 
 <style lang="less" rel="stylesheet/less" scoped>
-  /*@import "../../../common/css/index";*/
+  @import "../../../common/css/index";
 
+  .add-cart-buy {
+    .choose-all-img {
+
+    }
+    .buy-btn {
+      height: 40px;
+      font-size: 30px;
+      color: @white;
+      white-space: normal;
+      padding: 25px 90px;
+      background-image: linear-gradient(to right, @mainLef, @mainRight);
+    }
+  }
 </style>
