368642e8d49576384806d08e07e32d8b533ec395
liuxy0551 <liuxy0551@qq.com>
2018-09-13 22:13:54 +0800

    加入购物车-立即购买

--- web-calorie/CalorieH5/src/pages/meal/components/addCartBuy.vue
+++ web-calorie/CalorieH5/src/pages/meal/components/addCartBuy.vue
@@ -30,17 +30,32 @@
   @import "../../../common/css/index";
 
   .add-cart-buy {
+    width: 100%;
+    height: 77px;
+    display: flex;
+    padding-top: 13px;
+    background-color: @white;
+    position: fixed;
+    bottom: 0;
     .cart-img {
       width: 88px;
       height: 88px;
+      position: absolute;
+      left: 10px;
+      bottom: 40px;
     }
     .add-cart {
-
+      padding: 12px 65px;
+      margin-left: 170px;
+      border-radius: 40px 0 0 40px;
+      background-image: linear-gradient(to right, @mainLef, #5876E4);
     }
     .buy-now {
-
+      padding: 12px 65px;
+      border-radius: 0 40px 40px 0;
+      background-image: linear-gradient(to right, #314ad1, @mainRight);
     }
-    m-text {
+    .m-text {
       width: 150px;
       height: 40px;
       font-size: 30px;
